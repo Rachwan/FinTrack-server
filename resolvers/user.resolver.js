b0c5bc5b00b7f7c7341d34ca3702b18a8443a593
Rachwan <rachwan.harb2023@gmail.com>
2024-05-22 20:44:03 +0300

    Created the Query and Mutation functions for user

--- resolvers/user.resolver.js
+++ resolvers/user.resolver.js
@@ -1,14 +1,89 @@
 import { users } from '../data/data.js'
+import User from '../models/user.model.js'
+import bcrypt from 'bcrypt';
 const userResolver = {
   Query: {
-    users() {
-      return users
+    users: async () => {
+      try {
+        const users = await User.find({})
+        return users
+      } catch (error) {
+        console.error("Error: ", error.message)
+        throw new Error("Error getting users!")
+      }
     },
-    user: (_parent, { userId }) => {
-      return users.find((user) => user._id === userId)
-    }
+    user: async (_, { userId }) => {
+      try {
+        const user = await User.findById(userId)
+        if (!user) throw new Error("User not found!")
+        return user;
+      } catch (error) {
+        console.error("Error: ", error.message)
+        throw new Error("Error getting the user!")
+      }
+    },
+    authUser: async (_, _, context) => {
+      try {
+        const user = context.getUser();
+        return user
+      } catch (error) {
+        console.error("Error: ", error.message)
+        throw new Error("Error getting the auth user!")
+      }
+    },
   },
   Mutation: {
+    signUp: async (_, { input }, context) => {
+      try {
+        const { userName, name, password, gender } = input;
+        if (!userName || !name || !password || !gender) throw new Error("All fields are required!");
+        const user = await User.find({ userName })
+        if (user) throw new Error("Username already exist!");
+
+        const salt = bcrypt.genSalt(10);
+        const hashedPassword = bcrypt.hash(password, salt);
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${userName}`;
+
+        const newUser = new User({
+          userName,
+          name,
+          password: hashedPassword,
+          profilePicture: gender === "male" ? boyProfilePic : girlProfilePic
+        })
+
+        await newUser.save();
+        await context.login(newUser)
+      } catch (error) {
+        console.error("Error: ", error.message)
+        throw new Error("Error while signup!")
+      }
+    },
+    logIn: async (_, { input }, context) => {
+      try {
+        const { userName, password } = input;
+        const { user } = await context.authenticate("graphql-local", { userName, password })
+        await context.login(user)
+        return user
+      } catch (error) {
+        console.error("Error: ", error.message)
+        throw new Error("Error while login!")
+      }
+    },
+    logOut: async (_, _, context) => {
+      try {
+        await context.logout();
+        req.session.destroy((err) => {
+          if (err) throw new Error("Error: ", err.message)
+        })
+        res.clearCookie("connect.sid")
+        return { message: "Logout Successfully" }
+      } catch (error) {
+        console.error("Error: ", error.message)
+        throw new Error("Error while logout!")
+      }
+    }
   }
 }
 
